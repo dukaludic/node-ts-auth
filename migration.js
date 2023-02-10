4e4b6f41e01e17b3cb60fdba55cee4e156bf41c6
dukaludic <dukaludic@gmail.com>
2023-02-10 10:39:55 +0100

    docker

--- migration.js
+++ migration.js
@@ -3,15 +3,13 @@ var migration = require('mysql-migrations');
 
 require('dotenv').config();
 
-
-
-
 var connection = mysql2.createPool({
     connectionLimit: 10,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 // console.log(connection, __dirname + 'migrations');
